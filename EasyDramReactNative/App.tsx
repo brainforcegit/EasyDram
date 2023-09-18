862ffd7a83d78adffecad41eda80b04ea984b46c
brainforcegit <brainforceceo@gmail.com>
2023-09-18 09:42:16 +0000

    ui file structure  for Easy Dram

--- EasyDramReactNative/App.tsx
+++ EasyDramReactNative/App.tsx
@@ -1,13 +1,22 @@
-import React from 'react';
-
-import {Text, View} from 'react-native';
+import {StyleSheet, Text, View} from 'react-native';
 
 const App = (): JSX.Element => {
   return (
-    <View>
-      <Text>hell EasyDram</Text>
+    <View style={styles.container}>
+    
     </View>
   );
 };
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  greeting: {
+    fontSize: 35,
+    fontWeight: 'bold',
+  },
+});
 
 export default App;
---
+++ EasyDramReactNative/src/constants/images.ts
@@ -0,0 +1,9 @@
+import LogoSmall from '../../assets/images/Logo200x64.png';
+import LogoMedium from '../../assets/images/Logo200x200.png';
+import LogoLarge from '../../assets/images/Logo400x400.png';
+
+export default {
+LogoSmall,
+LogoMedium,
+LogoLarge,
+};
